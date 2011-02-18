bb0e5def760ff3b5f11efc64ec45cda59e5eec3f
Arun <arunkottooli@hotmail.com>
2011-02-18 08:30:26 +0530

    blog skeleton created

---
+++ apps/frontend/modules/welcome/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * welcome actions.
+ *
+ * @package    blog
+ * @subpackage welcome
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class welcomeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
