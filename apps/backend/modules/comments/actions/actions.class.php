d47388d1a2931cc09415479523a94ddc43cd3979
Arun <arunkottooli@hotmail.com>
2011-02-18 20:11:30 +0530

    blog created

--- apps/backend/modules/comments/actions/actions.class.php
+++ apps/backend/modules/comments/actions/actions.class.php
@@ -10,13 +10,64 @@
  */
 class commentsActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->forward('default', 'module');
+ public function executeIndex(sfWebRequest $request)
+   {
+     $this->comments = Doctrine::getTable('comment')->findAll();
+   }
+ 
+  public function executeNew(sfWebRequest $request)
+   {
+     $this->form = new commentForm();
+   }
+ 
+  public function executeCreate(sfWebRequest $request)
+   {
+     $this->form = new commentForm();
+     $this->processForm($request, $this->form);
+     $this->setTemplate('new');
+   }
+ 
+  public function executeEdit(sfWebRequest $request)
+   {
+     $this->form = new commentForm(Doctrine::getTable('comment')->findOneById($request->getParameter('id')));
+   }
+ 
+  public function executeUpdate(sfWebRequest $request)
+   {
+     $this->form = new commentForm(Doctrine::getTable('comment')->findOneById($request->getParameter('id')));
+     $this->processForm($request, $this->form);
+     $this->setTemplate('edit');
+   }
+ 
+  public function executeDelete(sfWebRequest $request)
+   {
+     $request->checkCSRFProtection();
+ 
+    try
+     {
+       $item = Doctrine::getTable('comment')->findById($request->getParameter('id'));
+       $item->delete();
+     }
+     catch(Exception $e){}
+ 
+    $this->redirect('@posts');
+ 
   }
+ 
+  public function executeShow(sfWebRequest $request)
+   {
+     $this->country = Doctrine::getTable('comment')->findOneById($request->getParameter('id'));
+   }
+ 
+  protected function processForm(sfWebRequest $request, sfForm $form)
+   {
+     $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+ 
+    if ($form->isValid())
+     {
+       $item = $form->save();
+       $this->redirect('@posts');
+     }
+   }
+ 
 }
--- apps/backend/modules/posts/templates/showSuccess.php
+++ apps/backend/modules/posts/templates/showSuccess.php
@@ -1,16 +1,16 @@
-<?php use_stylesheet('SimpleBlog') ?>
-
-   <div id="content-wrap">
-   <div id="main"> <a name="TemplateInfo"></a>
+<link type="text/css" rel="stylesheet" href="/css/Simple.css">
+  
+  <div id="main"> <a name="TemplateInfo"></a>
       <h1><?php echo $post->getTitle(); ?></h1>
       <p><?php echo $post->getBody(); ?> </p>
       <p class="post-footer align-right">   <span class="date"><?php echo $post->getCreatedAt(); ?></span> </p>
       <?php foreach($comments as $comment):?>
       <h3><?php echo $comment->getName(); ?></h3>
+     
       <blockquote>
         <p><?php echo $comment->getDescription(); ?></p>
       </blockquote>
+       <?php echo link_to('Delete', '@comments_delete?id='.$comment->getId().'&post_id='.$post->getId(), array('method' => 'delete', 'confirm' => __('Are you sure?'), 'class' => 'delete')); ?>
       <?php endforeach; ?>
-  
-   </div>
-   </div>
+  </div>
+ 
